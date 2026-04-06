05dce03557728aaa7b4ab882ae3faefb9830fed2
Kaustubh Shukla <kaustubhshukla9586@gmail.com>
2026-04-06 09:37:01 +0530

    fxn

--- Self/Chapter 7 - Arrays/practice/passing_to_fxn.c
+++ Self/Chapter 7 - Arrays/practice/passing_to_fxn.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+int printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+}
+
 int main() {
 
+    int arr[5] = {1,2,34,56,78};
+    printArray(arr,5);
     
-
     return 0;
 }
