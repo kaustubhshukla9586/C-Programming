81a456b19deb669342fd7801c723917d0c856e91
Kaustubh Shukla <kaustubhshukla9586@gmail.com>
2026-04-14 13:35:20 +0530

    pointer

--- Self/Chapter 6 - Pointers and its functions/pointer.c
+++ Self/Chapter 6 - Pointers and its functions/pointer.c
@@ -6,10 +6,14 @@ int main() {
     int *j = &i; // j is the pointer and gives the exact value of the address of i where it is pointing
     int k = 73;
 
+
+
     // we can use both %u which is unsigned int and %p which is pointer to display the address and with %u you can see the exact numerical value of the address of the pointer variable
 
     printf("The address of i is %u\n",j);
     printf("The address of i is %u\n",&k);
+    printf("Pointer size = %d",*j); // This gives 8 regardless of the datatype of the pointer variable 
+
 
     // To print the value from the address we can use this *(&varname)
     printf("The value at the address %u is %d",j,*(&i));
