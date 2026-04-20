786e853589b2523918fc95e62070e9ffa1bd0efb
Kaustubh Shukla <kaustubhshukla9586@gmail.com>
2026-04-20 13:44:26 +0530

    gg

--- Self/Chapter 3 - Operators/switchcase.c
+++ Self/Chapter 3 - Operators/switchcase.c
@@ -16,6 +16,33 @@ int main() {
     // default:
     // // c
     // }
+        
+    int x = 5;
+
+    switch (x)
+    {
+    case 3:
+    // code;
+    printf("h6elo");
+    case 4: 
+    // code; 
+    printf("h8elo\n");
+    case 2:
+    // code:
+    printf("h6elo\n");
+    case 5:
+    // code:
+    printf("he3lo\n");
+    case 6:
+    // code:
+    printf("h5elo\n");
+    case 7:
+    // code:
+    printf("hel3o\n");
+    default:
+    // c
+    printf("hel3o\n");
+    }
     
     return 0;
 }
